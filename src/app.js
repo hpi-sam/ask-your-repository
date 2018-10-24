3dea3023193603dd017d79fbd1bfcf29a0750db0
Jascha Beste <bestejascha@gmail.com>
2018-10-24 08:39:39 +0200

    Add post endpoint with logging.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // @flow
 import express from 'express';
 import type { $Request as Request, $Response as Response } from 'express';
+import logger from './logger';
 
 const app = express();
 
@@ -8,4 +9,8 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+app.post('/', (req: Request, res: Response) => {
+  logger.info(`Request ${req}...`);
+  res.send('Hello World!');
+});
 export default app;
