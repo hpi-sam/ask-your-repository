d1c0dca3d606d943aa3201000c48b731d23ab52f
Jascha Beste <jascha.beste@student.hpi.de>
2018-10-25 09:26:09 +0200

    Try actual fullfillment.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@
 import express from 'express';
 import type { $Request as Request, $Response as Response } from 'express';
 import bodyParser from 'body-parser';
+import { dialogflow, Image } from 'actions-on-google';
 import logger from './logger';
+
+// URLs for images used in card rich responses
+const architectureimage = 'https://cdn-images-1.medium.com/max/1600/1*TYRSuON0vVxy8olllrBVEw.png';
+const mindmapimage = 'https://www.zeitzuleben.de/zzlwp1892/wp-content/uploads/2001/02/Mind-Mapping-Aufbau-2.png';
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,15 +16,27 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.post('/', (req: Request, res: Response) => {
-  logger.info(`Requested paramters were: ${JSON.stringify(req.body.queryResult.parameters)}`);
-  logger.info(`Request ${JSON.stringify(req.body)}...`);
+const dialog = dialogflow();
 
-  const artifact = req.body.queryResult.parameters.Artifact;
-  // const date = req.body.queryResult.parameters;
+// Register handlers for Dialogflow intents
 
-  logger.info(`Artifact was: ${artifact}`);
-  // logger.info(`Date was: ${date}`);
-  res.send('Hello World!');
+dialog.intent('retrieve artifact', (conv, params) => {
+  logger.info(`parameters: ${conv.parameters}`);
+  logger.info(`Artifact: ${params.Artifact}`);
+  if (params.Artifact === 'Mindmap') {
+    conv.ask('Here is your Mindmap');
+    conv.ask(new Image({
+      url: mindmapimage,
+      alt: 'Your Mindmap',
+    }));
+  } else {
+    conv.ask('Here is your Architecture Sketch');
+    conv.ask(new Image({
+      url: architectureimage,
+      alt: 'Your Architecture Sketch',
+    }));
+  }
 });
+
+app.post('/', dialog);
 export default app;
