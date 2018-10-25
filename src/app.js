4a0030b792fb0275c7b7313be26c5055a2f18e3a
Jascha Beste <jascha.beste@student.hpi.de>
2018-10-25 08:39:21 +0200

    Add bodyparser to parse json requests.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 // @flow
 import express from 'express';
 import type { $Request as Request, $Response as Response } from 'express';
+import bodyParser from 'body-parser';
 import logger from './logger';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
