53c79a9c532a8b0b4470ff534fc71d0584d937ad
Jascha Beste <bestejascha@gmail.com>
2018-11-18 01:32:13 +0100

    Add nock nock mocks for elija calls. Ref #6

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -2,39 +2,36 @@
 import request from 'supertest';
 import fs from 'fs';
 import path from 'path';
-// import faker from 'faker';
-// import should from 'should';
+import nock from 'nock';
 import app from '../src/app';
 
 
 describe('app', () => {
-  describe('upload', () => {
-    let postRequest;
+  const elija = nock(process.env.ELIJA_URL);
     const uploadPath: string = process.env.UPLOAD_PATH;
 
-    before(() => {
-
-    });
-
+  describe('upload', () => {
     beforeEach(() => {
-      postRequest = request(app)
-        .post('/images')
-        .attach('image', 'spec/test.jpg');
+      elija.post('/artefacts/image').reply(200);
     });
 
     it('an image has status 200', (done) => {
-      postRequest.end((err, res) => {
-        expect(res.status).to.equal(200);
+      request(app).post('/images')
+        .attach('image', 'spec/test.jpg')
+        .end((err, res) => {
+          expect(res.status).to.equal(200); // 'success' status
         done();
       });
     });
 
-    it('an image creates an image file in the upload directory', (done) => {
+    it('an image creates an image file in /uploads/', (done) => {
       expect((stepDone) => {
-        postRequest.end(stepDone);
+        request(app).post('/images')
+          .attach('image', 'spec/test.jpg')
+          .end(stepDone);
       }).to.alter(() => fs.readdirSync(uploadPath).length, { by: 1, callback: done });
     });
-
+  });
     after(() => {
       fs.readdir(uploadPath, (err, files) => {
         files.forEach((file) => {
