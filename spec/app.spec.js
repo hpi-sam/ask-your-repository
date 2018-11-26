4e4dccebd78e144ff1e5e8c9b5d0ec3561f69952
Jascha Beste <bestejascha@gmail.com>
2018-11-26 12:03:59 +0100

    Add tests for error handling. Fix image response format. Ref #10

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -10,34 +10,125 @@ describe('app', () => {
   const elija = nock(process.env.ELIJA_URL);
   const uploadPath: string = process.env.UPLOAD_PATH;
 
+<<<<<<< HEAD
   describe('upload', () => {
     beforeEach(() => {
       elija.post('/artefacts').reply(200);
     });
+=======
+  describe('GET /images', () => {
+    context('valid request', () => {
+      let response;
+      beforeEach(async () => {
+        elija.get('/artefacts/image').reply(200, {
+          results:
+        [{
+          _id: 'db20072d-38dc-4226-83ff-f204b4366118',
+          _index: 'artefact',
+          _score: 0.5753642,
+          _source: {
+            created_at: '2018-11-14T12:41:26.754071',
+            file_url: 'class_diagram.png',
+            tags: ['uml', 'class diagram', 'architecture'],
+          },
+          _type: 'image',
+          sort: [
+            0.5753642,
+            1542199286754,
+          ],
+        },
+        {
+          _id: 'db20072d-38dc-4226-83ff-f204b4366118',
+          _index: 'artefact',
+          _score: 0.5753642,
+          _source: {
+            created_at: '2018-11-14T12:41:26.754071',
+            file_url: 'class_diagram.png',
+            tags: ['uml', 'class diagram', 'architecture'],
+          },
+          _type: 'image',
+          sort: [
+            0.5753642,
+            1542199286754,
+          ],
+        },
+        ],
+        });
+        response = await request(app).get('/images').set('Accept', 'application/json').send();
+      });
+>>>>>>> Add tests for error handling. Fix image response format. Ref #10
 
-    it('an image has status 200', (done) => {
-      request(app).post('/images')
-        .attach('image', 'spec/test.jpg')
-        .end((err, res) => {
-          expect(res.status).to.equal(200); // 'success' status
+      it('has status 200', (done) => {
+        expect(response.status).to.equal(200);
+        done();
+      });
+
+      it('warps image objects', (done) => {
+        expect(response.body.images.length).to.equal(2);
+        expect(response.body.images[0].url).to.equal('class_diagram.png');
+        expect(response.body.images[0].id).to.equal('db20072d-38dc-4226-83ff-f204b4366118');
+        // Only using eql to check for lose equality, since we don't care about object identity
+        expect(response.body.images[0].tags).to.eql(['uml', 'class diagram', 'architecture']);
+        done();
+      });
+    });
+    describe('possible errors', () => {
+      context('elija not connected', () => {
+        let response;
+        beforeEach(async () => {
+          response = await request(app).get('/images').set('Accept', 'application/json').send();
+        });
+        it('has status 503', (done) => {
+          expect(response.status).to.equal(503);
           done();
         });
-    });
-
-    it('an image creates an image file in /uploads/', (done) => {
-      expect((stepDone) => {
-        request(app).post('/images')
-          .attach('image', 'spec/test.jpg')
-          .end(stepDone);
-      }).to.alter(() => fs.readdirSync(uploadPath).length, { by: 1, callback: done });
+      });
     });
   });
-  after(() => {
-    // delete files in uploads directory
-    fs.readdir(uploadPath, (err, files) => {
-      files.forEach((file) => {
-        const filePath = path.join(uploadPath, file);
-        fs.unlinkSync(filePath);
+
+  describe('POST /Images (upload)', () => {
+    context('valid request', () => {
+      beforeEach(() => {
+        elija.post('/artefacts/image').reply(200);
+      });
+
+      it('has status 200', (done) => {
+        request(app).post('/images')
+          .attach('image', 'spec/test.jpg')
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            done();
+          });
+      });
+
+      it('creates an image file in /uploads/', (done) => {
+        expect((stepDone) => {
+          request(app).post('/images')
+            .attach('image', 'spec/test.jpg')
+            .end(stepDone);
+        }).to.alter(() => fs.readdirSync(uploadPath).length, { by: 1, callback: done });
+      });
+      after(() => {
+        // delete files in uploads directory
+        fs.readdir(uploadPath, (err, files) => {
+          files.forEach((file) => {
+            const filePath = path.join(uploadPath, file);
+            fs.unlinkSync(filePath);
+          });
+        });
+      });
+    });
+
+    describe('possible errors', () => {
+      context('elija not connected', () => {
+        let response;
+        beforeEach(async () => {
+          response = await request(app).get('/images').set('Accept', 'application/json').send();
+        });
+        it('has status 503', (done) => {
+          expect(response.status).to.equal(503);
+          done();
+        });
       });
     });
   });
--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -4,13 +4,20 @@ import type { $Request as Request, $Response as Response } from 'express';
 import ImageService from '../services/ImageService';
 import logger from '../logger';
 
+function formatIndex(elijaResponse) {
+  return elijaResponse.results.map(image => ({
+    id: image._id,
+    url: image._source.file_url,
+    tags: image._source.tags,
+  }));
+}
+
 export default {
   async upload(req: Request, res: Response) {
     const id = uuidv4();
     const path = `${process.env.FILE_SERVER}/${req.file.filename}`;
     try {
       await ImageService.create(id, '', path);
-      res.status(200).send({ id, path });
     } catch (err) {
       logger.error(err);
       if (err.response !== undefined) {
@@ -22,26 +29,24 @@ export default {
           },
         });
       } else {
-        res.status(500).send({
-          messages: ['Internal Error!'],
+        res.status(503).send({
+          messages: ['Database unavailable!'],
         });
       }
     }
+
+    res.status(200).send({ id, path });
   },
   async index(req: Request, res: Response) {
+    const searchTerm = req.params.search_term;
+    let response;
     try {
-      const searchTerm = req.params.search_term;
-      let response;
       if (searchTerm === '' || searchTerm === undefined) {
         response = await ImageService.listAll();
       } else {
         response = await ImageService.find(req.params.search_term);
       }
-      return res.status(200).send({
-        images: response.data,
-      });
     } catch (err) {
-      logger.error(err);
       if (err.response !== undefined) {
         return res.status(503).send({
           messages: ['Database Error!'],
@@ -51,9 +56,12 @@ export default {
           },
         });
       }
-      return res.status(500).send({
-        messages: ['Internal Error!'],
+      return res.status(503).send({
+        messages: ['Database unavailable!'],
       });
     }
+    return res.status(200).send({
+      images: formatIndex(response.data),
+    });
   },
 };
